47c14c62ced15bbbd9e59509f56f31a852c2531b
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-03-13 14:21:57 +0100

    Fix order of arguments in comment

--- skip.ts
+++ skip.ts
@@ -2,7 +2,7 @@ import Observable from './observable'
 import {Observable as ObservableT} from './index'
 
 // source s:   |--1--2--3--4--5--6--7--|
-// skip(3, s): |-----------4--5--6--7--|
+// skip(s, 3): |-----------4--5--6--7--|
 export default function skip<T>(source: ObservableT<T>, count: number): ObservableT<T> {
   return new Observable(({error, next, complete}) => {
     return source.subscribe({
