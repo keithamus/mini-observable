df4b53afe7677d235f9d1161c3500c6e7517cdc9
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-09-06 10:13:53 +0200

    style: apply strict rules to switchlatest.ts (#11)
    This applies strict typescript rules:
    
    - all arguments must have a name (TS7051)
    - ensure there is no implicit any for variables (TS7034)

--- switchlatest.ts
+++ switchlatest.ts
@@ -1,5 +1,5 @@
 import Observable from './observable'
-import {Observable as ObservableT} from './index'
+import {Observable as ObservableT, Subscription} from './index'
 
 // transform t: x => Observable.of(x+1, x+1, x+1))
 // source:       |--1-----2--3--4--------|
@@ -8,10 +8,10 @@ import {Observable as ObservableT} from './index'
 // source t(3):  |-----------4--4--4-----|
 // source t(4):  |--------------5--5--5--|
 // switchLatest: |--2--2--3--4--5--5--5--|
-export default function switchLatest<T, U>(source: ObservableT<T>, transform: (T) => ObservableT<U>): ObservableT<U> {
+export default function switchLatest<T, U>(source: ObservableT<T>, transform: (item: T) => ObservableT<U>): ObservableT<U> {
   let remaining = 0
   return new Observable(({error, next, complete}) => {
-    let oldSubscription = null
+    let oldSubscription: Subscription | null = null
     const {unsubscribe} = source.subscribe({
       error,
       next: value => {
