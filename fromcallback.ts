8be73c59f792612a6582de7a86fe7f2a8127c74e
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-09-06 10:13:00 +0200

    style: apply strict rules to fromcallback.ts (#9)
    This applies strict typescript rules:
    
    - ensure there is no rest arguments with implicit any[] (TS7019)

--- fromcallback.ts
+++ fromcallback.ts
@@ -1,6 +1,6 @@
 import Observable from './observable'
 import {Observable as ObservableT} from './index'
 
-export default function fromCallback<T>(func: Function): (...args) => ObservableT<T> {
+export default function fromCallback<T>(func: Function): (...args: any[]) => ObservableT<T> {
   return (...args) => new Observable(({next}) => func(...args.concat(next)))
 }
