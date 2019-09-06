6dce59d539a19e6305d10e80e2e3fcfcd9786e88
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-09-06 10:12:09 +0200

    style: apply strict rules to filter.ts (#7)
    This applies strict typescript rules:
    
    - all arguments must have a name (TS7051)

--- filter.ts
+++ filter.ts
@@ -4,7 +4,7 @@ import {Observable as ObservableT} from './index'
 // predicate p: x => x % 2 == 0
 // source s:     |--1--2--3--4--5--6--|
 // filter(p, s): |-----2-----4-----6--|
-export default function filter<T>(source: ObservableT<T>, predicate: (T) => boolean): ObservableT<T> {
+export default function filter<T>(source: ObservableT<T>, predicate: (item: T) => boolean): ObservableT<T> {
   return new Observable(({error, next, complete}) =>
     source.subscribe({
       error, complete,
